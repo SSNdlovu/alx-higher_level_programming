ee7bf3fe02e85d347746eca3f1b42a4eb9638b22
ssndlovu <ssndlovu101@gmail.com>
2023-06-14 19:36:35 +0000

    ssndlovu

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,5 +2,5 @@
 
 def update_dictionary(a_dictionary, key, value):
     """Replace or add value pairs in dict."""
-	a_dictionary[key] = value
-	return (a_dictionary)
+    a_dictionary[key] = value
+    return a_dictionary
