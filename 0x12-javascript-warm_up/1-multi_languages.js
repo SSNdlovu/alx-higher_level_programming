c2868806d9b734d4c2be4b0088c237c80adbfb99
ssndlovu <ssndlovu101@gmail.com>
2023-09-12 11:01:08 -0700

    Updated

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,10 @@
 !/usr/bin/node
 
-const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const lang of languages) console.log(lang);
+let line1 = "C is fun";
+let line2 = "Python is cool";
+let line3 = "JavaScript is amazing";
+
+// Printing the lines using console.log()
+console.log(line1);
+console.log(line2);
+console.log(line3);
