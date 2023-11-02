bb753ac54f08c4d2a58d1eee1722a295d712b4d8
SSNdlovu <ssndlovu101@gmail.com>
2023-11-01 21:12:29 -0700

    Updated

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,46 @@
+$(function () {
+  $("header").css("color", "#FF0000");
+});
+
+//OR
+//('header').css('color', '#FF0000');$(function () {
+//  $("header").css("color", "#FF0000");
+//  });
+//
+//  //OR
+//  //('header').css('color', '#FF0000');$(function () {
+//    $("header").css("color", "#FF0000");
+//    });
+//
+//    //OR
+//    //('header').css('color', '#FF0000');$(function () {
+//      $("header").css("color", "#FF0000");
+//      });
+//
+//      //OR
+//      //('header').css('color', '#FF0000');$(function () {
+//        $("header").css("color", "#FF0000");
+//        });
+//
+//        //OR
+//        //('header').css('color', '#FF0000');$(function () {
+//          $("header").css("color", "#FF0000");
+//          });
+//
+//          //OR
+//          //('header').css('color', '#FF0000');$(function () {
+//            $("header").css("color", "#FF0000");
+//            });
+//
+//            //OR
+//            //('header').css('color', '#FF0000');$(function () {
+//              $("header").css("color", "#FF0000");
+//              });
+//
+//              //OR
+//              //('header').css('color', '#FF0000');$(function () {
+//                $("header").css("color", "#FF0000");
+//                });
+//
+//                //OR
+//                //('header').css('color', '#FF0000');
